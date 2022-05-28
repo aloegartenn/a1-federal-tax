25ba98278e81ba16feda1b3702ee2da7c58cbb08
aloegartenn <97531085+aloegartenn@users.noreply.github.com>
2022-05-29 01:15:25 +0300

    few corrections

--- assets/app.js
+++ assets/app.js
@@ -24,9 +24,12 @@ else if(sum > 164925 && sum <= 209425){
 else if(sum > 209425 && sum <= 523600){
     tax = 9950 * 0.1 + ((sum - 9950) * 0.12) + ((sum-40525) * 0.22) + ((sum - 86375 ) * 0.24) + ((sum - 164925) * 0.32) + ((sum - 209425) * 0.35);
 }
-else{
+else if (sum > 523600){
     tax = 9950 * 0.1 + ((sum - 9950) * 0.12) + ((sum-40525) * 0.22) ((sum - 86375 ) * 0.24) + ((sum - 164925) * 0.32) + ((sum - 209425) * 0.35) + ((sum - 523600) * 0.37);
 }
 
-
+let results = sum - tax;
+results = Math.round(results);
+tax = Math.round(tax);
+console.log(`сумма налогов${tax}, чистый доход${results}`)
 
